2803ee04dee96cc0c659e7ee092aebe1ac4f153c
10km <10km0811@sohu.com>
2017-06-14 16:14:49 +0800

    增加编译选项，保证只编译静态库

--- bin/build_gflags.sh
+++ bin/build_gflags.sh
@@ -10,7 +10,13 @@ install_path=$GFLAGS_INSTALL_PATH
 echo install_path:$install_path
 pushd $SOURCE_ROOT/$GFLAGS_FOLDER
 remove_if_exist CMakeCache.txt
-$CMAKE_EXE . $CMAKE_VARS_DEFINE -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$install_path 
+$CMAKE_EXE . $CMAKE_VARS_DEFINE -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$install_path \
+	-DBUILD_SHARED_LIBS=off \
+	-DBUILD_STATIC_LIBS=on \
+	-DBUILD_gflags_LIB=on \
+	-DINSTALL_STATIC_LIBS=on \
+	-DINSTALL_SHARED_LIBS=off \
+	-DREGISTER_INSTALL_PREFIX=off
 exit_on_error
 remove_if_exist $install_path
 make clean
