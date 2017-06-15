6f29c8b6ecc1e3fbd00be0ec755e7f6baeb5e64b
10km <10km0811@sohu.com>
2017-06-15 18:31:13 +0800

    add functions for powershell

--- bin/build_funs.ps1
+++ bin/build_funs.ps1
@@ -1,56 +1,134 @@
-# 如果文件/文件夹存在则删除,删除失败则中止脚本
-Function remove_if_exist(){
-	param([string]$File)
-	if($args.Count -eq 1){
-		if(Test-Path $File){
-			del -Force -Recurse $File
-			if( $? -ne 0){
-				exit -1
-			}
-		}
-		return 0
-	}else {
-		# 参数错误，退出
-		echo invalid argument:
-		echo $*
+# debug开关
+$DebugPreference = "continue"
+
+#set-executionpolicy remotesigned 
+# 输出函数调用堆栈
+# $index 从堆栈数组的第几个元素开始输出，默认为1,即不输出当前函数(call_stack)
+function call_stack([int]$index=1){
+    [array]$stack=$(Get-PSCallStack)
+    $s2=$stack[$index..($stack.Count-1)]
+    echo "调用堆栈:"
+    echo  $s2 
+}
+# 变量名检查
+# 如果指定的变量名未定义或为空或null则报错退出脚本
+function args_not_null_empty_undefined(){
+    echo $args| foreach {
+        $name=$($_)
+        $varObj=(Get-Variable $name -ErrorAction SilentlyContinue)
+        if( $varObj -eq $null){            
+            echo "undefined variable: '$name'"
+            call_stack
+            exit -1
+        }
+        $value=$varObj.Value
+        Write-Debug "name:$name, value:$value"
+        if([string]::IsNullOrEmpty( $value)){
+            echo "the argument name '$name' must not be null or empty"
+            call_stack
+            exit -1
+        }           
+    }
+}
+# 上一条命令执行出错则中止脚本执行,并输出调用堆栈信息
+function exit_on_error(){
+	if ( ! $? ){
+		echo "exit for error:$1 " 
+        call_stack -index 2
 		exit -1
+	}
+}
+# 如果文件/文件夹存在则删除,删除失败则中止脚本
+function remove_if_exist([string]$file){
+	if(Test-Path $file){
+		del -Force -Recurse  $file
+		if( ! $? ){
+            call_stack 
+			exit -1
+		}
 	}
 }
 # 如果文件/文件夹存在返回 true,否则返回 false
-Function exit_file(){
-	param([string]$File)
-	if ( $args.Count -eq 1 ){
-		return Test-Path $File
-	}else{
-		# 参数错误，退出
-		echo invalid argument:
-		echo $*
-		exit -1
-	}
+function exist_file([string]$file,[Microsoft.PowerShell.Commands.TestPathType]$type="Any"){
+    args_not_null_empty_undefined file
+	return Test-Path "$file" -PathType $type
 }
 # 如果文件/文件夹不存在则报错退出
-Function exit_if_not_exist(){
-	param([string]$File,[string]$msg)
-	if([String]::IsNullOrEmpty()){
-		error_msg="not found: $File"
+function exit_if_not_exist([string]$file,[string]$msg,[Microsoft.PowerShell.Commands.TestPathType]$type="Any"){    
+	if($msg.Length -eq 0){
+        switch($type){
+            Leaf {$typeStr="file"}
+            Container {$typeStr="directory"}
+            Any {$typeStr=""}
+        }
+		$error_msg="not found $typeStr : $file"
 	}else{
-		error_msg=$msg
+		$error_msg=$msg
 	}
-	exit_file $File 
-	exit_on_error "$error_msg"
-}
-Function unzip_file()
-{
-    param([string]$ZipFile,[string]$TargetFolder)
-    #确保目标文件夹必须存在,目标文件夹存在 则清空文件夹
-    if(!(Test-Path $TargetFolder))
-    {
-        mkdir $TargetFolder
-    }else{
-    	del -Recurse -Force $TargetFolder\*
+	if(!$(exist_file $file -type $type)  ){
+        echo $error_msg
+        call_stack
+        exit -1
     }
-    $shellApp = New-Object -ComObject Shell.Application
-    $files = $shellApp.NameSpace($ZipFile).Items()
-    $shellApp.NameSpace($TargetFolder).CopyHere($files)
 }
-unzip_file -ZipFile d:\caffe-static\package\wgetwin-1_5_3_1-binary.zip -TargetFolder d:\caffe-static\tools\wget
+# 清除指定文件夹的内容，如果文件夹不存在则创建空文件夹
+function clean_folder([string]$folder){
+    args_not_null_empty_undefined folder
+    if(Test-Path $folder){
+    	del -Recurse -Force $folder\*
+    }else{
+        mkdir $folder
+    }
+	exit_on_error 
+}
+# 如果文件夹不存在则创建空文件夹
+function mkdir_if_not_exist([string]$folder){
+    args_not_null_empty_undefined folder
+	if (!(exist_file -file $folder -type Container)){	
+		mkdir $folder
+		exit_on_error
+	}
+}
+# 生成安装路径名后缀
+function install_suffix([string]$prefix){
+    args_not_null_empty_undefined prefix
+    try { 
+        # 先尝试执行linux命令判断操作系统,产生异常时执行windows下指令
+		$os=$(uname -s)
+		$processor=$(uname -p)
+    } catch { 
+        $os="windows"
+	    $arch=$env:PROCESSOR_ARCHITECTURE
+        $processor="x86"
+        if($arch -eq "AMD64"){
+             $processor += "_64"
+        }	    
+    } 
+   return "${prefix}_${os}_${processor}"
+}
+# 计算文件md5校验值
+function md5sum([string]$file){
+    exit_if_not_exist -file $file -type Leaf
+    return $(Get-FileHash $file -Algorithm MD5).Hash.ToLower()
+}
+function unzip_file([string]$zipFile,[string]$targetFolder)
+{
+    exit_if_not_exist $zipFile -type Leaf
+    # 检查是否为zip后缀
+    if(!$zipFile.EndsWith(".zip")){
+        echo "$zipFile not zip file"
+        call_stack
+        exit -1
+    }
+    # targetFolder为空时解压到zipFile同级文件夹的同名文件夹
+    if($targetFolder.Length -eq 0){
+        $src=$(Get-Item $zipFile)         
+        $targetFolder=$(Join-Path -ChildPath $src.BaseName -Path $src.DirectoryName)
+    }    
+    #确保目标文件夹必须存在,目标文件夹存在 则清空文件夹
+    clean_folder -folder $targetFolder
+    $shellApp = New-Object -ComObject Shell.Application
+    $files = $shellApp.NameSpace($zipFile).Items()
+	echo "unzip to $targetFolder..."
+    $shellApp.NameSpace($targetFolder).CopyHere($files)
+}
---
+++ bin/test1.ps1
@@ -0,0 +1,8 @@
+. "./build_funs.ps1"
+#set-executionpolicy remotesigned
+#echo (exist_file D:\j\axis2-1.6.2.zip )
+#exist_file 
+#exit_if_not_exist D:\j\a
+#unzip_file -zipFile D:\caffe-static\package\wgetwin-1_5_3_1-binary.zip 
+#install_suffix opencv
+md5sum ..\package\boost-1.58.0.tar.gz
