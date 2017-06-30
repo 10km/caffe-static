24208fd4d510b6bf10012bd743dc01ff66d4837a
10km <10km0811@sohu.com>
2017-06-30 11:55:24 +0800

    msvc编译debug版本时将Zi改为Z7,调试信息编译到obj文件中

--- bin/compiler_flag_overrides.cmake
+++ bin/compiler_flag_overrides.cmake
@@ -11,9 +11,20 @@ if(MSVC)
 		CMAKE_CXX_FLAGS_MINSIZEREL_INIT 
 		CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT
     )
+    set( has_replaced off)
     if(${var} MATCHES "/MD")
       string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
-      MESSAGE(STATUS  "${var}:${${var}}")
-    endif()    
+      set( has_replaced on)
+    endif()   
+    if(${var} MATCHES "/Z[iI]")
+    	# use /Z7 option to produces an .obj file containing full symbolic debugging information for use with the debugger
+    	# for detail,see https://msdn.microsoft.com/zh-cn/library/958x11bc.aspx
+      string(REGEX REPLACE "/Z[iI]" "/Z7" ${var} "${${var}}")
+      set( has_replaced on)
+    endif() 
+    if( has_replaced ) 
+    	MESSAGE(STATUS  "${var}:${${var}}")
+		endif( has_replaced )
+		unset( has_replaced )
   endforeach()	  
 endif(MSVC)
