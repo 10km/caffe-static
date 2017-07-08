74b25e67c1565b8493e2e79f8c57671945541552
10km <10km0811@sohu.com>
2017-07-08 09:33:22 +0800

    fix error install path

--- bin/build_lmdb_cmake.sh
+++ bin/build_lmdb_cmake.sh
@@ -12,10 +12,11 @@ pushd $SOURCE_ROOT/$LMDB_FOLDER/libraries/liblmdb
 clean_folder build.gcc
 pushd build.gcc
 $CMAKE_EXE .. $CMAKE_VARS_DEFINE -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$install_path \
-	-DBUILD_SHARED_LIBS=off 
+	-DBUILD_SHARED_LIBS=off \
+	-DBUILD_TEST=off 
 exit_on_error
 clean_folder $install_path
-make -j $MAKE_JOBS CC=$MAKE_C_COMPILER DESTDIR="$install_path" install 
+make -j $MAKE_JOBS install 
 popd
 rm -fr build.gcc
 popd
