f4256cd4d0ac087a6c329fa43d3cb34d5961981f
10km <10km0811@sohu.com>
2017-07-08 10:33:39 +0800

    add DYNAMIC_ARCH=1 for build openblas.

--- bin/build_OpenBLAS.sh
+++ bin/build_OpenBLAS.sh
@@ -13,13 +13,14 @@ make clean
 exit_on_error
 # NO_SHARED=1 不编译动态库
 # NOFORTRAN=1 不编译fortran
-make -j $MAKE_JOBS CC=$MAKE_C_COMPILER NOFORTRAN=1 NO_LAPACKE=1 NO_SHARED=1
+# DYNAMIC_ARCH=1 动态架构模式(根据cpu类型自动切换内核)
+make -j $MAKE_JOBS CC=$MAKE_C_COMPILER NOFORTRAN=1 NO_SHARED=1 DYNAMIC_ARCH=1
 if [  $? -ne 0 ]
 then
 		make clean
 		exit_on_error
 		# 如果编译出错，加上NO_AVX2=1再尝试一次,解决Error: no such instruction: `vpermpd` 
-		make -j $MAKE_JOBS CC=$MAKE_C_COMPILER NOFORTRAN=1 NO_LAPACKE=1 NO_SHARED=1 NO_AVX2=1
+		make -j $MAKE_JOBS CC=$MAKE_C_COMPILER NOFORTRAN=1 NO_SHARED=1 DYNAMIC_ARCH=1 NO_AVX2=1
 		exit_on_error
 fi
 
